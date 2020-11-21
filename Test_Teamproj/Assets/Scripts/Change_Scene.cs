3ab5fd28e40dc0ed7d680795243b684bf732079c
huntu0042 <huntu0042@hanmail.net>
2020-11-21 19:58:50 +0900

    first_ys

---
+++ Test_Teamproj/Assets/Scripts/Change_Scene.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Change_Scene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void SceneChange()
+    {
+        SceneManager.LoadScene("scene_stage1");
+    }
+
+    public void SceneChange_2()
+    {
+        SceneManager.LoadScene("scene_stage2");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
